836564da9a75563c493fce7b35c98adf2863ce0c
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-11 19:29:45 -0500

    Added edit file

---
+++ edit.php
@@ -0,0 +1,49 @@
+<html>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+
+
+
+<div class="container">
+  
+ 
+  
+  <form>
+   <div class="col-md-6">
+    <h4>Edit your profile</h4>
+  <div class="form-group">
+    <label for="exampleInputEmail1">First name</label>
+    <input type="text" class="form-control" id="First Name" aria-describedby="emailHelp" placeholder="Enter first name">
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Last name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter last name">
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="em" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+ 
+  
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+ <button type="submit" class="btn btn-primary">Save</button>
+
+  
+  </div>
+</form>
+  
+  
+  
+</div>
+
+
+
+
+</html>
