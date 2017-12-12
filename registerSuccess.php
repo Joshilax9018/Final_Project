59af4825227878c331d5220202f0e3ec07c67fab
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-11 19:36:35 -0500

    registration success

---
+++ registerSuccess.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+
+    <div>
+
+        <p>
+            Your Registration successful. Please use your email ID and Password for login.
+            <a href="login.php">Login</a>
+        </p>
+
+    </div>
+
+
+</body>
+
+
+</html>
