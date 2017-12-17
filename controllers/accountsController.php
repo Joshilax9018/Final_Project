ac73e31ebdfe4bf35c41c95f3bd8636d96e6a770
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-17 01:50:39 -0500

    changes

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -44,14 +44,23 @@ class accountsController extends http\controller
             $user->birthday = $_POST['birthday'];
             $user->gender = $_POST['gender'];
             //$user->password = $_POST['password'];
+            $user->password = $user->setPassword($_POST['password']);
+            echo $user->password;
+            $userID = $user->save();
+            
+            session_start();
+            $_SESSION["userID"] = $userID;
+            $_SESSION["userFname"] = $user->fname;
+            $_SESSION["userLname"] = $user->lname;
+            $_SESSION["userEmail"] = $user->email;
+          
             //this creates the password
             //this is a mistake you can fix...
             //Turn the set password function into a static method on a utility class.
-            $user->password = $user->setPassword($_POST['password']);
-            $user->save();
-            //you may want to send the person to a
-            // login page or create a session and log them in
-            // and then send them to the task list page and a link to create tasks
+            
+            
+              
+            
             header("Location: index.php?page=accounts&action=all");
         } else {
             //You can make a template for errors called error.php
@@ -68,6 +77,12 @@ class accountsController extends http\controller
     }
 //this is used to save the update form data
     public static function save() {
+    session_start();
+        if(key_exists('userID',$_SESSION)) {
+            $userID = $_SESSION['userID'];
+        } else {
+            header("Location: index.php?page=homepage&action=show");
+        }
         $user = accounts::findOne($_REQUEST['id']);
         $user->email = $_POST['email'];
         $user->fname = $_POST['fname'];
@@ -81,16 +96,12 @@ class accountsController extends http\controller
     public static function delete() {
         $record = accounts::findOne($_REQUEST['id']);
         $record->delete();
-        header("Location: index.php?page=accounts&action=all");
-    }
+        session_destroy();
+        header("Location: index.php?page=homepage&action=show");
+    }    }
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
-        //you will need to fix this so we can find users username.  YOu should add this method findUser to the accounts collection
-        //when you add the method you need to look at my find one, you need to return the user object.
-        //then you need to check the password and create the session if the password matches.
-        //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
-        //after you login you can use the header function to forward the user to a page that displays their tasks.
         //        $record = accounts::findUser($_POST['email']);
         $user = accounts::findUserbyEmail($_REQUEST['email']);
         if ($user == FALSE) {
@@ -100,8 +111,13 @@ class accountsController extends http\controller
                 echo 'login';
                 session_start();
                 $_SESSION["userID"] = $user->id;
-                //forward the user to the show all todos page
+                $_SESSION["userFname"] = $user->fname;
+                $_SESSION["userLname"] = $user->lname;
+                $_SESSION["userEmail"] = $user->email;
+                
+             //forward the user to the show all todos page
                 print_r($_SESSION);
+                header("Location: index.php?page=tasks&action=all");
             } else {
                 echo 'password does not match';
             }
@@ -111,5 +127,5 @@ class accountsController extends http\controller
     public static function logout()
     {
         session_destroy();
-        header('Location: index.php');
+        header("Location: index.php?page=homepage&action=show");
     }
