8d835f7624232dca9d20d541f010e19c7baeff7e
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-16 23:15:29 -0500

    changes made

--- core/model.php
+++ core/model.php
@@ -36,8 +36,17 @@ abstract class model
         $tableName = $modelName::getTablename();
         $array = get_object_vars($this);
         unset($array['id']);
-        $columnString = implode(',', array_flip($array));
-        $valueString = ':' . implode(',:', array_flip($array));
+        $columnString ='';
+        $valueString = '';
+        
+        foreach ($array as $key => $value) {
+            if($key != 'id' && $key != 'tableName'){
+                $columnString = $columnString . $key . ",";
+                $valueString = $valueString . "'" .$value . "',";
+            }
+        }
+        $columnString = rtrim($columnString, ',');
+        $valueString = rtrim($valueString, ',');
         $sql = 'INSERT INTO ' . $tableName . ' (' . $columnString . ') VALUES (' . $valueString . ')';
         return $sql;
     }
