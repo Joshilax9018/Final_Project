88836db45a6ae3accfe5f57fda7ce1d702fba344
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-17 04:49:50 -0500

    changes

--- database.php
+++ database.php
@@ -3,15 +3,3 @@ $servername = "sql2.njit.edu";
 $username = "svj28";
 $password = "vlAtaFzRh";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=svj28", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-   
-?>
