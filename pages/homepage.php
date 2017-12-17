b67768a2009416cb0e896fd6a4419668b9d7ec40
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-17 12:29:26 -0500

    updated

--- pages/homepage.php
+++ pages/homepage.php
@@ -27,14 +27,14 @@
 <h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
 <h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
 
-<form action="index.php?page=accounts&action=login" method="POST">
+<form action="index.php?page=accounts&action=login" method="POST" align='center'>
 
     <div class="container">
         <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
+        <input type="text" placeholder="Enter Username" name="email" required><br>
 
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+        <input type="password" placeholder="Enter Password" name="password" required><br>
 
         <button type="submit">Login</button>
     </div>
