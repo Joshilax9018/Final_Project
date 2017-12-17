bdc883e53e9c9fd71ef9b14b6e90cb0fe710dd0c
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-16 23:00:10 -0500

    register page

---
+++ pages/register.php
@@ -0,0 +1,61 @@
+<html>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+
+
+
+<div class="container">
+  
+ 
+  
+  <form>
+   <div class="col-md-6">
+
+    <h4>Sign Up</h4>
+  <div class="form-group">
+    <label for="exampleInputEmail1">First name</label>
+    <input type="text" class="form-control" id="First Name" aria-describedby="emailHelp" placeholder="Enter first name">
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Last name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter last name">
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Phone number</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter phone number">
+    
+    
+    
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Password</label>
+    <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password">
+ 
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Confirm password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+ <button type="submit" class="btn btn-primary">Sign Up</button>
+  <button type="submit" class="btn btn-primary">Login</button>
+  
+  </div>
+</form>
+  
+  
+  
+</div>
+
+
+
+
+</html>
